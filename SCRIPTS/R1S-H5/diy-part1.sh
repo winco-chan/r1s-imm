7bff2cc5c60dd5478d2f52509cf6eb0ed87af78f
winco-chan <winco1992@gmail.com>
2022-01-24 16:21:16 +0800

    update passwall

--- SCRIPTS/R1S-H5/diy-part1.sh
+++ SCRIPTS/R1S-H5/diy-part1.sh
@@ -11,8 +11,9 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*passwall\)/\1/' feeds.conf.default
 
 # Add a feed source
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
