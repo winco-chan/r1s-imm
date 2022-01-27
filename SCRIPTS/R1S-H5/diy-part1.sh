a44eb36e54f0dd19f63fd02249a345701cde8284
winco-chan <59270565+winco-chan@users.noreply.github.com>
2022-01-28 00:04:09 +0800

    Update diy-part1.sh

--- SCRIPTS/R1S-H5/diy-part1.sh
+++ SCRIPTS/R1S-H5/diy-part1.sh
@@ -12,12 +12,12 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*passwall\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*vlmcsd\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*openwrt-vlmcsd\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*vlmcsd\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*openwrt-vlmcsd\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git vlmcsd https://github.com/mchome/luci-app-vlmcsd' >>feeds.conf.default
-echo 'src-git penwrt-vlmcsd https://github.com/mchome/openwrt-vlmcsd' >>feeds.conf.default
+#echo 'src-git vlmcsd https://github.com/mchome/luci-app-vlmcsd' >>feeds.conf.default
+#echo 'src-git penwrt-vlmcsd https://github.com/mchome/openwrt-vlmcsd' >>feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
